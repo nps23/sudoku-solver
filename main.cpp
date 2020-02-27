403aa3a701076705b56ea3c306484c6d10640bc4
Nathanial Stodick <nathanialstodick@Nathanials-MacBook-Air.local>
2020-02-26 21:54:16 -0500

    updated driver code to solve one more problem

--- main.cpp
+++ main.cpp
@@ -46,6 +46,25 @@ int main()
     std::cout << "Solution to second puzzle:" << std::endl;
     puzzle2.PrintBoard();
 
+    std::vector<std::vector<int>> board3 = {
+        {0, 0, 6, 4, 8, 1, 3, 0, 0},
+        {0, 2, 0, 0, 0, 0, 4, 0, 0},
+        {7, 0, 0, 0, 0, 0, 0, 0, 9},
+        {8, 0, 0, 0, 9, 0, 0, 0, 4},
+        {6, 0, 0, 3, 4, 2, 0, 0, 1},
+        {5, 0, 0, 0, 6, 0, 0, 0, 2},
+        {3, 0, 0, 0, 0, 0, 0, 0, 5},
+        {0, 9, 0, 0, 0, 0, 0, 7, 0},
+        {0, 0, 5, 7, 1, 6, 2, 0, 0}
+    };
+
+    SudokuPuzzle puzzle3(board3);
+    std::cout << "Puzzle 3 is" << std::endl;
+    puzzle3.PrintBoard();
+
+    puzzle3();
+    std::cout << "Solution to puzzle 3 is " << std::endl;
+    puzzle3.PrintBoard(); 
 
     return 0;
 
